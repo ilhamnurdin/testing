d367f5c7a7d200e5d82fde1df53edd0a23708042
ilham <ilhamnurdin15@gmail.com>
2021-08-09 15:24:24 +0700

    dasar Navigasi Angular

--- test-angular/src/app/app-routing.module.ts
+++ test-angular/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'/login',
+    pathMatch:'full'
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'register',
+    component:RegisterComponent
+  },
+  {
+    path:'admin',
+    // memanggil module lain
+    loadChildren: () => import('./admin/admin.module').then(mod=>mod.AdminModule)
+  },
+  {
+    path:'public',
+    // memanggil module lain
+    loadChildren: () => import('./public/public.module').then(mod=>mod.PublicModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
